a71f1a0e8631209251170295f9c3cc0b2544fcf7
joshrizzo <josh.rizzo@gmail.com>
2014-10-02 17:42:15 -0500

    initial Commit

--- Readability/Controllers/HomeController.cs
+++ Readability/Controllers/HomeController.cs
@@ -14,37 +14,11 @@ namespace Readability.Controllers
 {
     public partial class HomeController : Controller
     {
-        private IBookRepo repo;
-
-        public HomeController(IBookRepo repo)
-        {
-            this.repo = repo;
-        }
-
         public virtual ActionResult Index()
         {
-            var booksFromXML = GetBooksFromXML();
-            var viewModel = ConstructViewModel(booksFromXML);
-            return View(viewModel);
-        }
-
-        private static List<HomeIndexViewModel> ConstructViewModel(List<Book> books)
-        {
-            var viewModel = new List<HomeIndexViewModel>();
-            foreach (var book in books)
-            {
-                viewModel.Add(new HomeIndexViewModel(book)
-                {
-                    IsInStock = book.Quantity <= 0,
-                    IsOld = book.Year < 1990
-                });
-            }
-            return viewModel;
-        }
-
-        private List<Book> GetBooksFromXML()
-        {
-            return repo.Books.ToList();
+            var data = new BookStoreContext().Books;
+            var model = new List<HomeIndexViewModel>();
+            return View();
         }
     }
 }
