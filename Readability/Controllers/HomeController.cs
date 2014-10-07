eccc03d37952550dbd80e29978eeccdae3a93348
joshrizzo <josh.rizzo@gmail.com>
2014-10-07 14:40:03 -0500

    Cleaned up comments and null check.

--- Readability/Controllers/HomeController.cs
+++ Readability/Controllers/HomeController.cs
@@ -15,24 +15,13 @@ namespace Readability.Controllers
     {
         public virtual ActionResult Index()
         {
-            // List for books from DB.
             var booksFromXML = GetBooksFromXML();
-
-            if (booksFromXML == null) 
-            {
-                throw new ApplicationException("No books found.");
-            }
-            
-            // Construct the View Model.
             var viewModel = ConstructViewModel(booksFromXML);
-
-            // Return the view and populate the model with the filtered list.
             return View(viewModel);
         }
 
         private static List<HomeIndexViewModel> ConstructViewModel(List<Book> booksFromXML)
         {
-            // Loop through the original list and filter certain results from the new list.  This is necessary because C# does not like us modifying the list that we are looping through.
             var viewModel = new List<HomeIndexViewModel>();
             foreach (var book in booksFromXML)
             {
@@ -48,28 +37,19 @@ namespace Readability.Controllers
         private List<Book> GetBooksFromXML()
         {
             var booksFromXML = new List<Book>();
-            // Get books from the XML file and turn them into Book objects. 
-            try
-            {
-                var bookElements = XDocument.Load(AppDomain.CurrentDomain.GetData("DataDirectory").ToString() + @"\BookData.xml")
-                    .Element("books").Elements("book").ToList();
-                foreach (var book in bookElements)
-                {
+            XDocument.Load(AppDomain.CurrentDomain.GetData("DataDirectory").ToString() + @"\BookData.xml")
+                .Element("books")
+                .Elements("book")
+                .ToList()
+                .ForEach(book => 
                     booksFromXML.Add(new Book()
                     {
                         Author = book.Element("author").Value,
                         Title = book.Element("title").Value,
                         Year = int.Parse(book.Element("year").Value),
                         Quantity = int.Parse(book.Element("quantity").Value)
-                    });
-                }
-            }
-            // Reading from files is dangerous, so lets catch any exeptions.
-            catch (Exception ex)
-            {
-                LogManager.GetLogger(typeof(HomeController)).Error("Stuff happened when loading the data from XML.", ex);
-                throw ex;
-            }
+                    })
+                );
             return booksFromXML;
         }
     }
